bb8ce8cae9955c395a1a941820ff409b1d88c140
Austin Song <austinhssong@gmail.com>
2023-06-08 10:42:10 -0400

    created game end logic, need to make modal trigger at game end

--- VanillaJS/app.js
+++ VanillaJS/app.js
@@ -10,6 +10,45 @@ const App = {
   state: {
     moves: [],
   },
+
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => {
+        return move.playerId === 1;
+      })
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => {
+        return move.playerId === 2;
+      })
+      .map((move) => +move.squareId);
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let winner = null;
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+      if (p1Wins) {
+        winner = 1;
+      }
+      if (p2Wins) {
+        winner = 2;
+      }
+    });
+    return {
+      status: moves.length === 9 || winner != null ? "Complete" : "In-Progress",
+      winner,
+    };
+  },
+
   init() {
     App.registerEventListeners();
   },
@@ -63,16 +102,14 @@ const App = {
         square.replaceChildren(icon);
 
         //Check for winner or tie
-        const winningPatterns = [
-          [1, 2, 3],
-          [1, 5, 9],
-          [1, 4, 7],
-          [2, 5, 8],
-          [3, 5, 7],
-          [3, 6, 9],
-          [4, 5, 6],
-          [7, 8, 9],
-        ];
+        const game = App.getGameStatus(App.state.moves);
+        if(game.status === 'Complete'){
+          if(game.winner){
+            alert(`Player ${game.winner} wins!`)
+          }else{
+            alert('Game ends in a tie!')
+          }
+        }
       });
     });
   },
