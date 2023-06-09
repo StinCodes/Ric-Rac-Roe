8598ad6ea3641b36d85cca6e561714f8e77a9e4c
Austin Song <austinhssong@gmail.com>
2023-06-09 09:36:47 -0400

    turn indicator completed

--- VanillaJS/app.js
+++ VanillaJS/app.js
@@ -9,6 +9,7 @@ const App = {
     modal: document.querySelector('[data-id= "modal"]'),
     modalText: document.querySelector('[data-id= "modal-text"]'),
     modalBtn: document.querySelector('[data-id= "modal-btn"]'),
+    turn: document.querySelector('[data-id= "turn"]'),
   },
   state: {
     moves: [],
@@ -89,20 +90,31 @@ const App = {
           App.state.moves.length === 0
             ? 1
             : getOppositePlayer(lastMove.playerId);
+        const nextPlayer = getOppositePlayer(currentPlayer)
 
-        const icon = document.createElement("i");
+        const squareIcon = document.createElement("i");
+        const turnIcon = document.createElement('i')
+        const turnLabel = document.createElement('p')
+        turnLabel.innerText = `Player ${nextPlayer} you are up!`
+
         if (currentPlayer === 1) {
-          icon.classList.add("fa-solid", "fa-x", "yellow");
+          squareIcon.classList.add("fa-solid", "fa-x", "yellow");
+          turnIcon.classList.add("fa-solid", "fa-o", "turquoise");
+          turnLabel.classList = 'turquoise'
         } else {
-          icon.classList.add("fa-solid", "fa-o", "turquoise");
+          squareIcon.classList.add("fa-solid", "fa-o", "turquoise");
+          turnIcon.classList.add("fa-solid", "fa-x", "yellow");
+          turnLabel.classList = 'yellow'
         }
+
+        App.$.turn.replaceChildren(turnIcon, turnLabel)
 
         App.state.moves.push({
           squareId: +square.id,
           playerId: currentPlayer,
         });
 
-        square.replaceChildren(icon);
+        square.replaceChildren(squareIcon);
 
         //Check for winner or tie
         const game = App.getGameStatus(App.state.moves);
